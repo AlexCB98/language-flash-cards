e85a78fa1f27803e4fcc678b11c2b85fd68eed70
Alexandru <lexyus22@gmail.com>
2026-06-17 17:54:14 +0200

    Add answer buttons

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ import tkinter as tk
 import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
+TITLE_FONT = ('Arial', 50, 'Italic')
+WORD_FONT = ('Arial', 60, 'Bold')
 
 window = tk.Tk()
 window.title('Flashcards - German/English')
-window.minsize(width=1000, height=700)
+window.minsize(width=900, height=640)
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = tk.Canvas(width=800, height=530, highlightthickness=0, bg=BACKGROUND_COLOR)
 card_front = tk.PhotoImage(file='images/card_front.png')
 canvas.create_image(400, 265, image=card_front)
-canvas.pack()
+canvas.grid(row= 0, column= 0, columnspan= 2)
+
+right_button_image = tk.PhotoImage(file= 'images/right.png')
+wrong_button_image = tk.PhotoImage(file= 'images/wrong.png')
+
+right_button = tk.Button(image= right_button_image, highlightthickness= 0)
+right_button.grid(row= 1, column= 1)
+
+wrong_button = tk.Button(image= wrong_button_image, highlightthickness= 0)
+wrong_button.grid(row= 1, column= 0)
+
 
 window.mainloop()
